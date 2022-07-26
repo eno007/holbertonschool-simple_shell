d75318b182c11d8e3f713b166b8f1ea48bbe2050
eno007 <eno.gupe@gmail.com>
2022-07-26 14:35:02 -0700

    update

--- sshell_exercises/for_wait_execve.c
+++ sshell_exercises/for_wait_execve.c
@@ -2,27 +2,22 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-/*
- * comment
- */
-int execute(int *forked)
+#include <stddef.h>
+int main(void)
 {
 	int status;
+	int forked = 0;
 	pid_t child_pid;
-	pid_t my_pid;
 	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
-
-	if (*forked != 5)
+	while (forked < 5)
 	{
 		child_pid = fork();
-		int status;
-
 		if (child_pid == -1)
 		{
 			perror("Error:");
 			return (1);
 		}
-		else if (child_pid != 0)
+		else if (child_pid == 0)
 		{
 			if (execve(argv[0], argv, NULL) == -1)
 				perror("Error:");
@@ -30,21 +25,8 @@ int execute(int *forked)
 		else
 		{
 			wait(&status);
-			*forked += 1;
-			execute(forked);
+			forked++;
 		}
 	}
-	my_pid = getpid();
-
-	return (0);
-}
-/*
- * comment
- */ 
-int main(void)
-{
-	int forked = 0;
-
-	execute(&forked);
 	return (0);
 }
