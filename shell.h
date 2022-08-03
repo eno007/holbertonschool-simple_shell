9ed66e9d7cfa46abd3357a7140f4ea5f4382273b
eno007 <eno.gupe@gmail.com>
2022-08-03 12:39:44 -0700

    update

--- shell.h
+++ shell.h
@@ -28,4 +28,7 @@ int command_read(char *str, size_t __attribute__((unused))characters);
 int main(int __attribute__ ((unused))argc, char *argv[]);
 int execute(char *cmd_arr[]);
 
+char **parse_cmd(char *input);
+unsigned int check_delim(char c, const char *str);
+char *_strtok(char *str, const char *delim);
 #endif
