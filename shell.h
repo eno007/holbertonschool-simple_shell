842b3e421d1a2f2cc4fc1b74818c6b53b7d04d8c
eno007 <eno.gupe@gmail.com>
2022-08-03 10:47:53 -0700

    update

--- shell.h
+++ shell.h
@@ -1,40 +1,62 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#ifndef _SHELL_H_
+#define _SHELL_H_
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+/*==================================================*/
+/*============     Shell_Init       ==============*/
+/*==================================================*/
 
-extern char **environ;
+int main(int ac, char **av, char **env);
+void prompt(void);
+void handle(int signals);
+void _EOF(char *buffer);
+void shell_exit(char **command);
 
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-int _strncmp(char *s1, char *s2, int n);
-char *_strdup(char *str);
+/*==================================================*/
+/*============     create_child       ==============*/
+/*==================================================*/
 
-int _printenv(void);
-char *_getenv(const char *name);
+void create_child(char **command, char *name, char **env, int cicles);
+int change_dir(const char *path);
 
-int command_read(char *s, size_t __attribute__((unused))characters);
-int execute(char *cmd_arr[]);
-int main(int __attribute__ ((unused))argc, char *argv[]);
+/*==================================================*/
+/*============        Execute       ==============*/
+/*==================================================*/
 
-char *_strtok(char *str, const char *delim);
-unsigned int check_delim(char c, const char *str);
+void execute(char **command, char *name, char **env, int cicles);
+void print_env(char **env);
+char **_getPATH(char **env);
+void msgerror(char *name, int cicles, char **command);
 
-char *command_path(char *cmd);
+/*==================================================*/
+/*============          Tokening      ==============*/
+/*==================================================*/
 
+char **tokening(char *buffer, const char *s);
+
+/*==================================================*/
+/*============     Free Memory      ==============*/
+/*==================================================*/
+
+void free_dp(char **command);
 void free_exit(char **command);
-void _exit_();
+
+/*==================================================*/
+/*============  Auxiliar_Functions    ==============*/
+/*==================================================*/
+
+int _strcmp(char *s1, char *s2);
+unsigned int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_strcat(char *dest, char *src);
 
+/*============ END      ==============*/
 
-
-#endif
+#endif /* _SHELL_H_ */
