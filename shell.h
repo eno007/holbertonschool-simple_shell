b3fdb8db1cd2fb0599fab09dad023d9251b3976d
eno007 <eno.gupe@gmail.com>
2022-08-05 13:33:11 -0700

    update

--- shell.h
+++ shell.h
@@ -1,5 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define ERR_MALLOC "Unable to malloc space\n"
+#define ERR_FORK "Unable to fork and create child process\n"
+#define ERR_PATH "No such file or directory\n"
+#define BUFFER 1024
+#define TRUE 1
+#define PROMPT "$ "
 
 #include <string.h>
 #include <stdio.h>
@@ -25,6 +31,13 @@ int _str_n_cmp(const char *s1, const char *s2, size_t n);
 int _printenv(void);
 char *command_path(char *cmd);
 char *_getenv(char *name);
+
+int shell_exit(void);
+int shell_env(void);
+void errors(int error);
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
+void free_dp(char **array, unsigned int length);
+char **tokenizer(char *str);
 
 int command_read(char *str, size_t __attribute__((unused))characters);
 int main(int __attribute__ ((unused))argc, char *argv[]);
